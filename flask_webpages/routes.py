508cdb353c7336b444b40bcb26251eeec3b83889
efishbein33 <fishbeinetai@gmail.com>
2019-10-06 22:58:08 -0800

    update existing file

--- flask_webpages/routes.py
+++ flask_webpages/routes.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
+# basic route with a URL pattern and a handler function
+''' @app.route('/')
 def index():
-    return 'Hello World!'
+    return 'Hello Flask' '''
+
+# route that uses a dynamic URL pattern
+@app.route('/user/<int:user_id>/')
+def user_profile(user_id):
+    return f"Profile page of user #{user_id}"
+
+# another route that uses a dynamic URL pattern but does not use a converter
+@app.route('/books/<genre>/')
+def books(genre):
+    return f"All Books in {genre} category"
+
+@app.route('/')
+def requestdata():
+    ''' prints the ip address and user agent of the client '''
+    return f'''Hello! <br><br> <b> Your IP address is:<b>
+                {request.remote_addr} <br>  <b>And you are using:</b> {request.user_agent}.'''
