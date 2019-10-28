89f7b8ebfc5606fb395de275bb9b66f38da9d3bc
efishbein33 <fishbeinetai@gmail.com>
2019-10-27 22:23:50 -0800

    connecting multiple URLs in one view function

--- flask_webpages/multiple_urls_in_one_view_function.py
+++ flask_webpages/multiple_urls_in_one_view_function.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
+# mapped both URL's 'contact' and 'feedback' to the same view function of 'Feedback Page'
 @app.route('/contact/')
 @app.route('/feedback/')
 def feedback():
