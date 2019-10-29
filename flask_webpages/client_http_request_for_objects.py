4e9540b76c0d606a66e6dc4cc61ffae214455588
efishbein33 <fishbeinetai@gmail.com>
2019-10-29 09:02:55 -0800

    new file

---
+++ flask_webpages/client_http_request_for_objects.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+'''
+any request made by the cliuent is passed through a flask object.
+showing how the handler functions work to represent the request made by the client
+and return the correct response needed.
+'''
+
+@app.route('/')
+def requestdata():
+    ''' prints the IP address and uder agent of the client '''
+    return f'''Hello! <br><br> <b> Your IP address is:</b>
+            {request.remote_addr} <br> <b> And you are using:</b> {request.user_agent}.'''
