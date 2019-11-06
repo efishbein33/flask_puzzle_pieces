6d39f918720d21d872460efdb239089d817d64fc
efishbein33 <fishbeinetai@gmail.com>
2019-11-06 09:32:10 -0900

    updated file names

--- flask_webpages/connect_routes_to_html/building_a_navbar.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    ''' returns the index template, which includes the navbar template '''
-    return render_template('index.html')
